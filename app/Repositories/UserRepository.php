503f91153592169a911f254353c4c8e6fe92e15b
Raúl García <raul.garcia@opensource.gt>
2025-06-28 21:54:08 -0600

    Cambios en el repositorio de usuarios

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class UserRepository implements UserRepositoryInterface
 {
     public function all()
@@ -11,7 +12,7 @@ class UserRepository implements UserRepositoryInterface
     }
     public function findbyEmail($email)
     {
-     return User::where('email',$id)->all();   
+     return User::where('email',$email)->get();   
     }
 
     public function find($id)
@@ -21,28 +22,28 @@ class UserRepository implements UserRepositoryInterface
 
     public function create(array $data)
     {
+        $password_temporal = "123456789";
         $user = User::create([
             'name' => $data["name"],
             'email' => $data["email"],
-            'password' => Hash::make($data["password"]),
+            'password' => Hash::make($password_temporal),
         ]);
         return $user;
     }
 
     public function update($id, array $data)
     {
+        
         $post = User::findOrFail($id);
         $post->name = $data["name"];
         $post->email = $data["email"];
-        if ( $data["password"]!= "")
-            $post->password = Hash::make($data["password"]);
-        $post->update($data);
+        $post->save();
         return $post;
     }
 
     public function delete($id)
     {
-        $post = Opcion::findOrFail($id);
+        $post = User::findOrFail($id);
         $post->delete();
         return true;
     }
